3369328455c1c4e2c9103c619a0f0b635d54e79d
vector-of-bool <vectorofbool@gmail.com>
2019-11-14 02:34:40 -0700

    Utility fns for pulling tuples from queries

---
+++ src/neo/sqlite3/next.test.cpp
@@ -0,0 +1,22 @@
+#include <neo/sqlite3/database.hpp>
+#include <neo/sqlite3/next.hpp>
+
+#include <catch2/catch.hpp>
+
+TEST_CASE("Pull the next result") {
+    auto db = neo::sqlite3::create_memory_db();
+    db.prepare("CREATE TABLE foo AS VALUES (1, 2, 3)").run_to_completion();
+    auto st           = db.prepare("SELECT * FROM foo");
+    auto [i1, i2, i3] = neo::sqlite3::unpack_next<int, int, int>(st);
+    CHECK(i1 == 1);
+    CHECK(i2 == 2);
+    CHECK(i3 == 3);
+    CHECK(st.is_busy()); // Still waiting
+    CHECK(st.step() == neo::sqlite3::statement::done);
+}
+
+TEST_CASE("Multiple results causes failure") {
+    auto db = neo::sqlite3::create_memory_db();
+    db.prepare("CREATE TABLE foo AS VALUES (1, 2, 3), (4, 5, 6)").run_to_completion();
+    auto st = db.prepare("SELECT * FROM foo");
+}
