a37f80f9759db70dfd4b844cb2f78fc8846c63d6
vector-of-bool <vectorofbool@gmail.com>
2020-04-03 13:48:52 -0600

    Separate types for recursive transaction guards

--- src/neo/sqlite3/transaction.hpp
+++ src/neo/sqlite3/transaction.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <optional>
 #include <utility>
 
 namespace neo::sqlite3 {
@@ -8,31 +9,58 @@ class database;
 
 class transaction_guard {
     int       _n_uncaught_exceptions = 0;
-    bool      _dropped               = false;
     database* _db                    = nullptr;
-    bool      _is_topmost            = false;
 
 public:
     explicit transaction_guard(database& db);
     ~transaction_guard();
 
-    transaction_guard(transaction_guard&& other)
+    transaction_guard(transaction_guard&& other) noexcept
         : _n_uncaught_exceptions(other._n_uncaught_exceptions)
-        , _dropped(other._dropped)
         , _db(std::exchange(other._db, nullptr)) {}
 
     transaction_guard& operator=(transaction_guard&& other) noexcept {
         std::swap(_n_uncaught_exceptions, other._n_uncaught_exceptions);
-        std::swap(_dropped, other._dropped);
         std::swap(_db, other._db);
         return *this;
     }
 
     void commit();
     void rollback();
-    void drop() noexcept { _dropped = true; }
+    void drop() noexcept { _db = nullptr; }
 
-    bool is_top_transaction() const noexcept { return _is_topmost; }
+    bool dropped() const noexcept { return _db == nullptr; }
+};
+
+class recursive_transaction_guard {
+    // We just wrap a transaction guard. This optional will only be engaged if
+    // we are the top-level transaction.
+    std::optional<transaction_guard> _inner;
+
+public:
+    explicit recursive_transaction_guard(database& db);
+
+    void commit() {
+        if (is_top_transaction()) {
+            _inner->commit();
+        }
+    }
+
+    void rollback() {
+        if (is_top_transaction()) {
+            _inner->rollback();
+        }
+    }
+
+    void drop() noexcept {
+        if (is_top_transaction()) {
+            _inner->drop();
+        }
+    }
+
+    bool dropped() const noexcept { return !_inner.has_value() || _inner->dropped(); }
+
+    bool is_top_transaction() const noexcept { return _inner.has_value(); }
 };
 
 }  // namespace neo::sqlite3
