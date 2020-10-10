01d106d3e3a70e55c2f6ae9bc5e17e9571a5ca9d
vector-of-bool <vectorofbool@gmail.com>
2020-10-09 23:59:35 -0600

    Significant cleanup in exec:
    - Just accept plain statements. Caller should do their own prep
      and caching.

--- src/neo/sqlite3/exec.hpp
+++ src/neo/sqlite3/exec.hpp
@@ -1,75 +1,72 @@
 #pragma once
 
-#include <neo/sqlite3/database.hpp>
 #include <neo/sqlite3/iter_rows.hpp>
 #include <neo/sqlite3/iter_tuples.hpp>
-#include <neo/sqlite3/literal.hpp>
 #include <neo/sqlite3/statement.hpp>
-#include <neo/sqlite3/statement_cache.hpp>
+
+#include <neo/fwd.hpp>
 
 #include <tuple>
 
 namespace neo::sqlite3 {
 
-inline void exec(database& db, const std::string& query) { db.exec(query); }
-
+/**
+ * @brief Reset, rebind, and execute a prepared statement to completion.
+ *
+ * Yielded rows are discarded.
+ *
+ * @param st The statement to execute
+ * @param bindings The bindings to apply to the prepared statement
+ */
 template <typename... Ts>
-void exec(database& db, std::string_view query, const std::tuple<Ts...>& bindings) {
-    auto st     = db.prepare(query);
-    st.bindings = bindings;
+void exec(statement& st, Ts&&... bindings) {
+    st.reset();
+    st.bindings.clear();
+    st.bindings = std::forward_as_tuple(NEO_FWD(bindings)...);
     st.run_to_completion();
 }
 
-inline void exec(statement& st) {
-    st.reset();
-    st.bindings.clear();
-    st.run_to_completion();
+// R-value statement overload
+template <typename... Ts>
+void exec(statement&& st, Ts&&... bindings) {
+    exec(st, NEO_FWD(bindings)...);
 }
 
+/**
+ * @brief Reset and rebind a prepared statement. Returns a range of rows.
+ *
+ * @param st The statement to execute
+ * @param bindings The parameter bindings for the prepared statement
+ * @return A range of row objects.
+ */
 template <typename... Ts>
-void exec(statement& st, const std::tuple<Ts...>& bindings) {
+[[nodiscard]] auto exec_rows(statement& st, Ts&&... bindings) {
     st.reset();
     st.bindings.clear();
-    st.bindings = bindings;
-    st.run_to_completion();
-}
-
-template <typename... Ts>
-void exec(statement_cache& stc, sql_string_literal sql, const std::tuple<Ts...>& bindings) {
-    statement& st = stc(sql);
-    st.reset();
-    st.bindings.clear();
-    st.bindings = bindings;
-    st.run_to_completion();
-}
-
-inline void exec(statement_cache& stc, sql_string_literal sql) { exec(stc, sql, std::tuple<>()); }
-
-template <typename... Ts>
-auto exec_iter(statement_cache& stc, sql_string_literal sql, const std::tuple<Ts...>& bindings) {
-    statement& st = stc(sql);
-    st.reset();
-    st.bindings.clear();
-    st.bindings = bindings;
+    st.bindings = std::forward_as_tuple(NEO_FWD(bindings)...);
     return iter_rows(st);
 }
 
-template <typename... OutTypes, typename... Ts, typename = std::enable_if_t<sizeof...(OutTypes)>>
-auto exec_iter(statement_cache& stc, sql_string_literal sql, const std::tuple<Ts...>& bindings) {
-    statement& st = stc(sql);
+// R-value statement overload
+template <typename... Ts>
+[[nodiscard]] auto exec_rows(statement&& st, Ts&&... bindings) {
+    return exec_rows(st, NEO_FWD(bindings)...);
+}
+
+/**
+ * @brief Reset and rebind a prepared statement. Returns a range of tuples.
+ *
+ * @tparam OutTypes The types of the tuple elements to pull from the result rows
+ * @param st The statement to execute
+ * @param bindings The parameter bindings for the prepared statement
+ * @return A range of tuples corresponding to the values in the rows
+ */
+template <typename... OutTypes, typename... Ts>
+[[nodiscard]] auto exec_tuples(statement& st, Ts&&... bindings) {
     st.reset();
     st.bindings.clear();
-    st.bindings = bindings;
+    st.bindings = std::forward_as_tuple(NEO_FWD(bindings)...);
     return iter_tuples<OutTypes...>(st);
-}
-
-inline auto exec_iter(statement_cache& stc, sql_string_literal sql) {
-    return exec_iter(stc, sql, std::tuple<>());
-}
-
-template <typename... OutTypes, typename = std::enable_if_t<sizeof...(OutTypes)>>
-auto exec_iter(statement_cache& stc, sql_string_literal sql) {
-    return exec_iter<OutTypes...>(stc, sql, std::tuple<>());
 }
 
 }  // namespace neo::sqlite3
