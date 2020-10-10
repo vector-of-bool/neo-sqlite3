f6eeabbcc8e16b854d951e86b16817968a4725d5
vector-of-bool <vectorofbool@gmail.com>
2020-10-10 17:57:18 -0600

    Disable more warnings for inline SQLite code

--- src/neo/sqlite3/c/sqlite3.c
+++ src/neo/sqlite3/c/sqlite3.c
@@ -6,6 +6,9 @@
 #define SQLITE_ENABLE_COLUMN_METADATA NEO_FeatureIsEnabled(neo_sqlite3, ColumnMetadata)
 
 #ifdef __GNUC__
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#pragma GCC diagnostic ignored "-Wfloat-conversion"
 #pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
 #pragma GCC diagnostic ignored "-Wcast-function-type"
 #endif
