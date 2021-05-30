e685299a1f702829a50109fc1fed0434684058df
vector-of-bool <vectorofbool@gmail.com>
2021-05-30 17:15:19 -0600

    Cleaner errable<> interface

--- src/neo/sqlite3/errable.hpp
+++ src/neo/sqlite3/errable.hpp
@@ -25,7 +25,7 @@ class errable_base {
     ::sqlite3*  _dbptr   = nullptr;
 
 public:
-    constexpr explicit errable_base(enum errc rc) noexcept
+    constexpr errable_base(enum errc rc) noexcept
         : _rc(rc) {}
     errable_base(enum errc rc, const database_ref& db) noexcept;
     errable_base(enum errc rc, const char* context, const database_ref& db) noexcept;
@@ -33,54 +33,38 @@ public:
         : _rc(rc)
         , _context{context} {}
 
+    constexpr bool is_error() const noexcept {
+        using e = neo::sqlite3::errc;
+        return _rc != e::ok && _rc != e::row && _rc != e::done;
+    }
+
+    constexpr explicit operator bool() const noexcept { return !is_error(); }
+
     constexpr enum errc errc() const noexcept { return _rc; }
 
     [[noreturn]] void throw_error() const;
-};
 
-inline constexpr struct errant_t {
-} errant;
-
-template <>
-class [[nodiscard]] errable<void> : errable_base {
-    bool _is_error = false;
-
-public:
-    constexpr errable() noexcept
-        : errable_base(neo::sqlite3::errc::ok) {}
-
-    constexpr errable(errant_t, enum errc rc) noexcept
-        : errable_base(rc)
-        , _is_error(true) {}
-
-    constexpr errable(errant_t, enum errc rc, const char* context) noexcept
-        : errable_base(rc, context)
-        , _is_error(true) {}
-
-    errable(errant_t, enum errc rc, const database_ref& db) noexcept
-        : errable_base(rc, db)
-        , _is_error(true) {}
-
-    errable(errant_t, enum errc rc, const char* context, const database_ref& db) noexcept
-        : errable_base(rc, context, db)
-        , _is_error(true) {}
-
-    using errable_base::errc;
-    using errable_base::throw_error;
-
-    constexpr explicit operator bool() const noexcept { return bool(_is_error); }
-
-    void throw_if_error() const {
-        if (_is_error) {
+    constexpr void throw_if_error() const {
+        if (is_error()) {
             throw_error();
         }
     }
+};
+
+template <>
+class [[nodiscard]] errable<void> : public errable_base {
+
+public:
+    using errable_base::errable_base;
+    using errable_base::errc;
+    using errable_base::throw_error;
 };
 
 template <typename T>
 class errable : errable_base {
     // The return value:
     neo::nano_opt<T> _value;
+    struct in_place_t {};
 
 public:
     template <std::convertible_to<T> Arg>
@@ -88,24 +72,18 @@ public:
         : errable_base(neo::sqlite3::errc::ok)
         , _value((Arg &&)(arg)) {}
 
-    template <std::convertible_to<T> Arg>
-    constexpr errable(enum errc           rc,
-                      const database_ref& db,
-                      Arg&& arg) noexcept(noexcept(std::is_nothrow_convertible_v<Arg, T>))
+    template <typename... Args>
+    requires std::constructible_from<T, Args&&...> && (sizeof...(Args) > 0)  //
+        constexpr errable(enum errc rc, Args&&... args) noexcept(
+            noexcept(std::is_nothrow_constructible_v<T, Args&&...>))
         : errable_base(rc)
-        , _value((Arg &&)(arg)) {}
+        , _value(in_place_t{}, (Args &&)(args)...) {}
 
     using errable_base::errable_base;
     using errable_base::errc;
     using errable_base::throw_error;
 
-    void throw_if_error() const {
-        if (!_value.has_value()) {
-            throw_error();
-        }
-    }
-
-    constexpr explicit operator bool() const noexcept { return bool(_value); }
+    constexpr bool has_value() const noexcept { return _value.has_value(); }
 
     constexpr T* operator->() noexcept {
         throw_if_error();
@@ -138,6 +116,8 @@ public:
 
     DECL_GETTER(value, (), ());
     DECL_GETTER(operator*,(), ());
+
+#undef DECL_GETTER
 };
 
 }  // namespace neo::sqlite3
