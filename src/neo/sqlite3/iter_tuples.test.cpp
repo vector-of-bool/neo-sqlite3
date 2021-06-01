9053061daee9579a9c516f4629512d308c573317
vector-of-bool <vectorofbool@gmail.com>
2021-05-31 21:43:17 -0600

    Test for using iter_tuples<> with a transform and auto-reset

--- src/neo/sqlite3/iter_tuples.test.cpp
+++ src/neo/sqlite3/iter_tuples.test.cpp
@@ -1,7 +1,9 @@
 #include <neo/sqlite3/iter_tuples.hpp>
 
-#include "./tests.inl"
 #include "./statement.hpp"
+#include "./tests.inl"
+
+#include <neo/memory.hpp>
 
 #if __has_include(<ranges>)
 #include <ranges>
@@ -42,4 +44,36 @@ TEST_CASE_METHOD(sqlite3_memory_db_fixture, "Iterate over some tuples") {
         ++iter;
     }
     CHECK(iter == stop);
+}
+
+TEST_CASE_METHOD(sqlite3_memory_db_fixture, "Create a transformed-view over some tuples") {
+    db.exec(R"(
+        CREATE TABLE stuff
+        AS VALUES
+            (1, 2, 'string'),
+            (2, 5, 'other'),
+            (5, 2, 'meow')
+    )")
+        .throw_if_error();
+    auto st = *db.prepare("SELECT * FROM stuff");
+    {
+        struct thing {
+            int         a;
+            int         b;
+            std::string str;
+
+            bool operator==(const thing&) const = default;
+        };
+        auto view = neo::sqlite3::iter_tuples<int, int, std::string>(st)
+            | std::views::transform([rst = neo::copy_shared(st.auto_reset())](auto tup) {
+                        auto [a, b, str] = tup;
+                        return thing{a, b, str};
+                    });
+        auto it = view.begin();
+        CHECK(*it == thing{1, 2, "string"});
+        ++it;
+        CHECK(*it == thing{2, 5, "other"});
+    }
+    // We didn't advance to the end, but the statement will be reset by the auto-reset
+    CHECK_FALSE(st.is_busy());
 }
