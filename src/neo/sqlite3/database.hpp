5d80f9ad19aa5d6b26ab28599ebb01e1ef847796
vector-of-bool <vectorofbool@gmail.com>
2021-06-11 19:18:28 -0600

    Guard against older neo-fun

--- src/neo/sqlite3/database.hpp
+++ src/neo/sqlite3/database.hpp
@@ -2,7 +2,7 @@
 
 #include <neo/platform.hpp>
 
-#if !__dds_header_check
+#if !__dds_header_check && defined(NEO_PRAGMA_WARNING)
 NEO_PRAGMA_WARNING("This file is deprecated. Use <neo/sqlite3/connection.hpp> instead.");
 #endif
 
