04c62179ba34f02a7dad5e677b09c5b9071fd205
vector-of-bool <vectorofbool@gmail.com>
2021-11-26 23:06:17 -0700

    iter_rows::iterator should throw, not assert

--- src/neo/sqlite3/iter_rows.cpp
+++ src/neo/sqlite3/iter_rows.cpp
@@ -6,14 +6,12 @@ using namespace neo::sqlite3;
 
 iter_rows::iterator::iterator(statement& st)
     : _st(&st) {
-    auto rc = st.step();
-    neo_assert(invariant, rc == errc::row || rc == errc::done, "Initial step returned an error");
+    st.step().throw_if_error();
 }
 
 void iter_rows::iterator::increment() {
     neo_assert(expects, _st != nullptr, "Advanced of row-iterator with no associated statement");
     neo_assert(expects, !at_end(), "Advance of a finished row-iterator");
-    auto rc = _st->step();
-    neo_assert(invariant, rc == errc::row || rc == errc::done, "step() returned an error");
+    _st->step().throw_if_error();
 }
 bool iter_rows::iterator::at_end() const noexcept { return !_st->is_busy(); }
